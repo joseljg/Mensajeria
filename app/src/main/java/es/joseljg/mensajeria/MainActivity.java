2ea9fccaa6b52874d9f344de1e70324bc4049cf7
joseljg <joseljg@gmail.com>
2021-10-01 21:14:44 +0200

    version11

--- app/src/main/java/es/joseljg/mensajeria/MainActivity.java
+++ app/src/main/java/es/joseljg/mensajeria/MainActivity.java
@@ -2,13 +2,27 @@ package es.joseljg.mensajeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_TEXTO1 = "es.joseljg.MainActivity.texto1";
+
+    private EditText edt_enviado1 = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edt_enviado1 = (EditText) findViewById(R.id.edt_enviado1);
+    }
+
+    public void enviar(View view) {
+        String texto = String.valueOf(edt_enviado1.getText());
+        Intent intent = new Intent(this, segundoActivity.class);
+        intent.putExtra(EXTRA_TEXTO1,texto);
+        startActivity(intent);
     }
 }
--- app/src/main/java/es/joseljg/mensajeria/segundoActivity.java
+++ app/src/main/java/es/joseljg/mensajeria/segundoActivity.java
@@ -2,13 +2,27 @@ package es.joseljg.mensajeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class segundoActivity extends AppCompatActivity {
+
+    private TextView txt_recibido2 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
+        txt_recibido2 = (TextView) findViewById(R.id.txt_recibido2);
+        txt_recibido2.setVisibility(View.VISIBLE);
+        //-------------------------------------------------------------
+        Intent intent = getIntent();
+        if(intent != null)
+        {
+            String texto = intent.getStringExtra(MainActivity.EXTRA_TEXTO1);
+            txt_recibido2.setText(texto);
+        }
     }
 }
